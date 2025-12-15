fd258e41064fb1efc1a7de762aa6e6d5b23a3124
Mahmoudtarek944 <mahmod122tarek@gmail.com>
2025-12-15 15:41:39 +0200

    add footer

--- src/component/pages/SearchExercises.jsx
+++ src/component/pages/SearchExercises.jsx
@@ -23,14 +23,14 @@ function SearchExercises() {
 
     fetchExerciesData();
 
-    // async function handelData() {
-    //   const exercisesData = await getData(
-    //     `https://exercisedb.p.rapidapi.com/exercises`,
-    //     exerciseOptions
-    //   );
-    //   setExercises(exercisesData);
-    // }
-    // handelData();
+    async function handelData() {
+      const exercisesData = await getData(
+        `https://exercisedb.p.rapidapi.com/exercises`,
+        exerciseOptions
+      );
+      setExercises(exercisesData);
+    }
+    handelData();
   }, []);
 
   async function handelData() {
--- src/component/share/Footer.jsx
+++ src/component/share/Footer.jsx
@@ -1,5 +1,17 @@
+import footerImg from "../../assets/Logo-1.png";
 function Footer() {
-  return <></>;
+  return (
+    <>
+      <div className="py-3 mt-5 container d-flex flex-column gap-5 w-100 align-items-center text-center footer-bg rounded-4">
+        <img src={footerImg} alt="" className="w-25 " />
+        <h3 className="text-black">
+          <pre>
+            Made With <i class="bi bi-heart"></i> by Mahmoud Tarek{" "}
+          </pre>
+        </h3>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
